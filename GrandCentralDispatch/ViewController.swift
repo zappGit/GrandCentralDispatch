73d6bf1e3120f25ce6a6fb6b61f7ba0c2d163328
Artyom Khrebtov <t.khrebtov@yandex.ru>
2021-07-17 21:35:44 +0500

    Initial Commit

---
+++ GrandCentralDispatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrandCentralDispatch
+//
+//  Created by Артем Хребтов on 17.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
