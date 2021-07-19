685d99094cfe723740d03c92c24e5c9caf8cab08
Artyom Khrebtov <t.khrebtov@yandex.ru>
2021-07-19 16:00:10 +0500

    add After

--- GrandCentralDispatch/SecondViewController.swift
+++ GrandCentralDispatch/SecondViewController.swift
@@ -17,8 +17,8 @@ class SecondViewController: UIViewController {
             return imageView.image
         }
         set {
-            activityIndicator.isHidden = true
             activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
             imageView.image = newValue
             imageView.sizeToFit()
         }
@@ -27,15 +27,50 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getImage()
+        delay(delay: 5) {
+            self.loginAlert()
+        }
         
+        
+    }
+    
+    func loginAlert() {
+        let ac = UIAlertController(title: "Регистрейшн?", message: "Введите логин и пассворд", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        ac.addTextField { loginTF in
+            loginTF.placeholder = "Введите логин плз"
+        }
+        ac.addTextField { passwordTF in
+            passwordTF.placeholder = "Введите пассворд плз"
+            passwordTF.isSecureTextEntry = true
+        }
+        ac.addAction(ok)
+        ac.addAction(cancel)
+        present(ac, animated: true)
+    }
+    
+    func delay (delay: Int, closure: @escaping () -> ()){
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
     }
     
     func getImage() {
         let imageURL = URL(string: "https://src.photogenica.ru/400/PHA/0004/PHA0004687.jpg")
         activityIndicator.isHidden = false
         activityIndicator.startAnimating()
-        guard let image = imageURL, let imageData = try? Data(contentsOf: image) else { return }
-        self.image = UIImage(data: imageData)
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let image = imageURL, let imageData = try? Data(contentsOf: image) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+            
+        }
+            
+        
+       
     }
     
     
